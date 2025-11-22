366e8649c55d1476856ac59532ebb98f714fa8b2
Ilia <code.crafter.front@gmail.com>
2025-11-23 01:14:31 +0300

    version 1

--- src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import counterReducer from './reducer';
-
-const store = createStore(counterReducer);
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export default store;
